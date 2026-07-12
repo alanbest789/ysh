13760a3fbb52e194d48081cd12b64e613faa0879
Yijun Liu <bill.liu@sperobill.com>
2026-07-12 15:48:49 -0400

    Update BrowserRouter basename to use import.meta.env

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename={process.env.CLIENT_BASE_PATH || "/"}>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AppContainer>
         <ErrorBoundary
           fallbackRender={({ error, resetErrorBoundary }) => (
