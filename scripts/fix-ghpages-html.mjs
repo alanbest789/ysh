dc9b02d21174a7ea7798bd9416e75d2a04b09b38
Yijun Liu <bill.liu@sperobill.com>
2026-07-14 03:44:18 -0400

    Preserve the 迎胜汇 logo in GitHub Pages builds

--- scripts/fix-ghpages-html.mjs
+++ scripts/fix-ghpages-html.mjs
@@ -3,4 +3,10 @@ import { readFile, writeFile } from "node:fs/promises";
 const indexPath = new URL("../dist/index.html", import.meta.url);
 const html = await readFile(indexPath, "utf8");
 
-await writeFile(indexPath, html.replaceAll("{{appName}}", "迎胜汇"));
+await writeFile(
+  indexPath,
+  html
+    .replaceAll("{{appName}}", "迎胜汇")
+    .replaceAll("{{{appAvatar}}}", "/logo.png")
+    .replaceAll("{{appAvatar}}", "/logo.png"),
+);
