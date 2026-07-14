171b979391dee06abecf88e6809ff8f503d8bb20
Yijun Liu <bill.liu@sperobill.com>
2026-07-14 03:42:01 -0400

    Add GitHub Pages title postprocessor

---
+++ scripts/fix-ghpages-html.mjs
@@ -0,0 +1,6 @@
+import { readFile, writeFile } from "node:fs/promises";
+
+const indexPath = new URL("../dist/index.html", import.meta.url);
+const html = await readFile(indexPath, "utf8");
+
+await writeFile(indexPath, html.replaceAll("{{appName}}", "迎胜汇"));
